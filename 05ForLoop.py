a973fe1276a15a825449a58df8e30175f89b1a80
isabellepannell <isabelle.pannell@education.nsw.gov.au>
2021-10-27 16:16:11 +1100

    done

---
+++ 05ForLoop.py
@@ -0,0 +1,9 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05HelloWorld                                |")
+print("|    Name : Isabelle Pannell                     |")
+print("|    Version : 01                                |")
+print("|                                                |") 
+print(" ------------------------------------------------")
+for i in range(500):
+    print(i)
