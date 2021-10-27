566e8a859836f0ff3db3997a0c7fcc95e823eb79
isabellepannell <isabelle.pannell@education.nsw.gov.au>
2021-10-27 17:33:00 +1100

    done i think

---
+++ 06whileLoop.py
@@ -0,0 +1,12 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Isabelle Pannell                     |")
+print("|    Version : 01                                |")
+print("|                                                |") 
+print(" ------------------------------------------------")
+user = input("What is the name of this subject ")
+if user == "IST": 
+    exit()
+else:
+    print("Not Correct - try again")
