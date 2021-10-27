40f8a518458ac0e4469f97d266ee63444f8c7502
isabellepannell <isabelle.pannell@education.nsw.gov.au>
2021-10-27 16:10:56 +1100

    done

---
+++ 04GoodTeacher.py
@@ -0,0 +1,12 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Isabelle Pannell                     |")
+print("|    Version : 01                                |")
+print("|                                                |") 
+print(" ------------------------------------------------")
+user = input("Teacher's name (try Mr Horan) ")
+if user == "Mr Horan": 
+    print("You are lucky, he is a great teacher.")
+else:
+    print(user + " is an ok teacher")
